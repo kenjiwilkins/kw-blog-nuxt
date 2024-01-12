e8484c0eeddd2d7d909a4372a9169850953dc983
Michael Kenji Wilkins <kenji.wilkins@gmail.com>
2024-01-12 16:37:02 +0900

    add tailwind and header

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss"],
+  runtimeConfig: {
+    // The private keys which are only available server-side
+    apiSecret: "test",
+    // Keys within public are also exposed client-side
+    public: {
+      apiBase: "/api",
+    },
+  },
+});
